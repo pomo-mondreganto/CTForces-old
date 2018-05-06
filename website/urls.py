5993931051c58a7cdcad859dd05dddc7db93065c
pomo-mondreganto <nikrom2012@me.com>
2018-05-06 16:48:24 +0300

    Fixed urls

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 
 from .views import UserRegistrationView, UserLoginView, UserInformationView, MainView, test_view, debug_view
 
@@ -6,7 +7,7 @@ urlpatterns = [
     url('^$', MainView.as_view(), name='main_view'),
     url('^signup/$', UserRegistrationView.as_view(), name='signup'),
     url('^signin/', UserLoginView.as_view(), name='signin'),
-    url('^user/<username>', UserInformationView.as_view(), name='user_info'),
+    path('user/<str:username>', UserInformationView.as_view(), name='user_info'),
     url('^test', test_view, name='test_view'),
     url('^debug', debug_view, name='debug_view'),
 ]
