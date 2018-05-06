b9aff3b586d29809c375833ef22fc51be23bd0c2
pomo-mondreganto <nikrom2012@me.com>
2018-05-06 16:53:42 +0300

    Removed aliased url function from urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import UserRegistrationView, UserLoginView, UserInformationView, MainView, test_view, debug_view
 
 urlpatterns = [
-    url('^$', MainView.as_view(), name='main_view'),
-    url('^signup/$', UserRegistrationView.as_view(), name='signup'),
-    url('^signin/', UserLoginView.as_view(), name='signin'),
-    path('user/<str:username>', UserInformationView.as_view(), name='user_info'),
-    url('^test', test_view, name='test_view'),
-    url('^debug', debug_view, name='debug_view'),
+    re_path('^$', MainView.as_view(), name='main_view'),
+    re_path('^signup/$', UserRegistrationView.as_view(), name='signup'),
+    re_path('^signin/$', UserLoginView.as_view(), name='signin'),
+    path('user/<str:username>/', UserInformationView.as_view(), name='user_info'),
+    re_path('^test', test_view, name='test_view'),
+    re_path('^debug', debug_view, name='debug_view'),
 ]
