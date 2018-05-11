b0c42c51a374d2dc9a4b26fe7315caea87de3522
pomo-mondreganto <nikrom2012@me.com>
2018-05-11 23:40:34 +0300

    Added custom image size validation

--- website/models.py
+++ website/models.py
@@ -1,11 +1,14 @@
 import os
 import uuid
+from io import BytesIO
 
+from PIL import Image
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.validators import BaseValidator
 from django.db import models
 from django_countries.fields import CountryField
 from stdimage.models import StdImageField
-from stdimage.validators import MinSizeValidator
 
 
 # Create your models here.
@@ -39,6 +42,60 @@ class CustomUploadTo:
         return path, self.args, self.kwargs
 
 
+class CustomImageSizeValidator(BaseValidator):
+
+    def __init__(self, min_limit, max_limit, ratio):
+        self.min_limit = min_limit
+        self.max_limit = max_limit
+        self.ratio_limit = ratio
+
+    @staticmethod
+    def clean(value):
+        value.seek(0)
+        stream = BytesIO(value.read())
+        img = Image.open(stream)
+        return img.size
+
+    def __call__(self, value):
+        cleaned = self.clean(value)
+        if self.compare_min(cleaned, self.min_limit):
+            params = {
+                'width': self.min_limit[0],
+                'height': self.min_limit[1],
+            }
+            raise ValidationError('''Your image is too small. 
+                                     The minimal resolution is {width}x{height}'''.format(**params),
+                                  code='min_resolution')
+        if self.compare_max(cleaned, self.max_limit):
+            params = {
+                'width': self.max_limit[0],
+                'height': self.max_limit[1],
+            }
+            raise ValidationError('''Your image is too big. 
+                                     The maximal resolution is {width}x{height}'''.format(**params),
+                                  code='max_resolution')
+
+        if self.compare_ratio(cleaned, self.ratio_limit):
+            params = {
+                'ratio': self.ratio_limit,
+            }
+            raise ValidationError('''Your image is too unbalanced. 
+                                     The maximal ratio is {ratio}'''.format(**params),
+                                  code='ratio')
+
+    @staticmethod
+    def compare_min(img_size, min_size):
+        return img_size[0] < min_size[0] or img_size[1] < min_size[1]
+
+    @staticmethod
+    def compare_max(img_size, max_size):
+        return img_size[0] > max_size[0] or img_size[1] > max_size[1]
+
+    @staticmethod
+    def compare_ratio(img_size, ratio):
+        return img_size[0] * ratio <= img_size[1] or img_size[1] * ratio <= img_size[0]
+
+
 class User(AbstractUser):
     organization = models.ForeignKey(
         'Organization',
@@ -64,7 +121,9 @@ class User(AbstractUser):
                                'main': (300, 300),
                                'small': (100, 100)
                            },
-                           validators=[MinSizeValidator(150, 150)],
+                           validators=[CustomImageSizeValidator(min_limit=(150, 150),
+                                                                max_limit=(1500, 1500),
+                                                                ratio=2)],
                            default='avatars/default_image.jpg',
                            blank=False, null=False)
 
