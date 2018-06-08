7575db715b10eaf73f7072d28a985d31d15cba1d
pomo-mondreganto <1728390Roman>
2018-06-08 18:27:06 +0300

    Contests model added Upcoming contests added to context

--- website/context_processors.py
+++ website/context_processors.py
@@ -1,6 +1,15 @@
-from .models import User
+from django.utils import timezone
+
+from .models import User, Contest
 
 
 def top_users(request):
     users = User.objects.all().order_by('-rating')[:10]
     return {'top_users': users}
+
+
+def upcoming_contests(request):
+    min_time = timezone.now()
+    max_time = min_time + timezone.timedelta(weeks=14)
+    contests = Contest.objects.filter(datetime__gt=min_time, datetime__lte=max_time)
+    return {'upcoming_contests': contests}
