67e0a4d28410e05568e66e128ab03c61102eba73
pomo-mondreganto <1728390Roman>
2018-06-08 20:46:26 +0300

    Fixed upcoming contests

--- website/context_processors.py
+++ website/context_processors.py
@@ -11,5 +11,5 @@ def top_users(request):
 def upcoming_contests(request):
     min_time = timezone.now()
     max_time = min_time + timezone.timedelta(weeks=14)
-    contests = Contest.objects.filter(datetime__gt=min_time, datetime__lte=max_time)
+    contests = Contest.objects.filter(datetime__gt=min_time, datetime__lte=max_time).all()
     return {'upcoming_contests': contests}
