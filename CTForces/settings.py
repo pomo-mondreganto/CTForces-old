340dc3c3fe38dd110a1dbf5a442af0eb248b12be
pomo-mondreganto <1728390Roman>
2018-06-15 21:59:01 +0300

    Added redis as cache engine

--- CTForces/settings.py
+++ CTForces/settings.py
@@ -256,3 +256,16 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'django.contrib.auth.backends.AllowAllUsersModelBackend'
 ]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "CTForces"
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
