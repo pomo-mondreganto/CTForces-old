c4b05b0735028c47e6698370fea0287204c69675
Ilya <i.digidi@mail.ru>
2018-06-17 21:28:25 +0300

    make everything smaller

--- static/css/css_analyzer.py
+++ static/css/css_analyzer.py
@@ -1,71 +1,76 @@
-f = open("main.css", "r")
-si = f.readlines()
-f.close()
+def file2code(filename):
 
-s = ""
+	f = open(filename, "r")
+	si = f.readlines()
+	f.close()
 
-for i in si:
-	was_ch = False
-	for j in i:
-		if j == "	" or j == "\r" or j == "\n" or (j == " " and not was_ch):
-			pass
+	s = ""
+
+	for i in si:
+		was_ch = False
+		for j in i:
+			if j == "	" or j == "\r" or j == "\n" or (j == " " and not was_ch):
+				pass
+			else:
+				was_ch = True
+				s += j
+
+	op = False
+	pr = False
+
+	cn = ""
+	pn = ""
+	vn = ""
+
+	code = []
+
+	obj = [None, []]
+	for ch in s:
+		if ch == '{':
+			op = True
+			obj[0] = cn
+			cn = ""
+		elif ch == '}':
+			op = False
+			code.append(obj)
+			obj = [None, []]
+		elif ch == ":" and op:
+			pr = True
+		elif ch == ";" and op:
+			pr = False
+			obj[1].append([pn, vn])
+			pn = ""
+			vn = ""
 		else:
-			was_ch = True
-			s += j
+			if not op:
+				cn += ch
+			elif not pr:
+				pn += ch
+			else:
+				vn += ch
 
-op = False
-pr = False
+	for i in range(len(code)):
+		code[i][1].sort()
 
-cn = ""
-pn = ""
-vn = ""
+	return code
 
-code = []
+def code2file(filename, code):
+	f = open(filename, "w")
+	for i in code:
+		f.write(i[0] + " {\n")
+		for j in i[1]:
+			f.write("	" + j[0] + ":" + j[1] + ";\n")
+		f.write("}\n\n")
+	f.close()
 
-obj = [None, []]
-for ch in s:
-	if ch == '{':
-		op = True
-		obj[0] = cn
-		cn = ""
-	elif ch == '}':
-		op = False
-		code.append(obj)
-		obj = [None, []]
-	elif ch == ":" and op:
-		pr = True
-	elif ch == ";" and op:
-		pr = False
-		obj[1].append([pn, vn])
-		pn = ""
-		vn = ""
-	else:
-		if not op:
-			cn += ch
-		elif not pr:
-			pn += ch
-		else:
-			vn += ch
+code = file2code("cp.css")
 
-for i in range(len(code)):
-	code[i][1].sort()
+for i in code:
+	for j in i[1]:
+		if j[1][-2:] == "vw":
+			num = float(j[1][1:-2])
+			num /= 1.2
+			j[1] = " " + str(num) + "vw"
 
-d = {}
 
-for i in range(len(code)):
-	key = str(code[i][1])
-	value = code[i][0]
-	if key in d:
-		d[key].append(value)
-	else:
-		d[key] = [value]
-
-for i in d:
-	if len(d[i]) > 1:
-		for j in d[i]:
-			print(j)
-		print("\n")
-
-#f = open("new_main.css", "w")
-#f.write(str(d))
-#f.close()
+code2file("main.css", code)
