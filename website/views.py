082a26c2e227402d4ef8b69298ef9a579f134a24
Ilya <i.digidi@mail.ru>
2018-06-19 09:55:36 +0300

    add latex

--- website/views.py
+++ website/views.py
@@ -26,8 +26,7 @@ from .view_classes import GetPostTemplateViewWithAjax, UsernamePagedTemplateView
 
 
 def test_view(request):
-    post = Post.objects.get(id=4)
-    return render(request=request, template_name='test.html', context={"post": post})
+    return render(request=request, template_name='test.html')
 
 
 def debug_view(request):
