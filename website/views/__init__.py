dc30a37eb08dd8fced9682da254ce195d389e00a
pomo-mondreganto <1728390Roman>
2018-06-26 23:21:16 +0300

    Added tasks solved by user view && url

--- website/views/__init__.py
+++ website/views/__init__.py
@@ -4,6 +4,7 @@ from .others_views import test_view, debug_view
 from .posts_views import UserBlogView, PostCreationView, PostView
 from .posts_views import leave_comment
 from .tasks_views import TaskView, TaskCreationView, TasksArchiveView
+from .tasks_views import UserSolvedTasksView
 from .tasks_views import UserTasksView, TaskEditView, TaskSolvedView
 from .tasks_views import search_tags, submit_task
 from .users_views import FriendsView, UserTopView, PasswordResetEmailView
